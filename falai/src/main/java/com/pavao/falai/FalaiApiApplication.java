c97fcb2adc7dca43169fc8065394a938477eb869
Gustavo Pavão <luisgustavopavao@hotmail.com>
2025-03-21 17:05:43 -0300

    start project and `docker compose`

---
+++ falai/src/main/java/com/pavao/falai/FalaiApiApplication.java
@@ -0,0 +1,13 @@
+package com.pavao.falai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalaiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalaiApiApplication.class, args);
+	}
+
+}
---
+++ falai/src/test/java/com/pavao/falai/FalaiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavao.falai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalaiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
